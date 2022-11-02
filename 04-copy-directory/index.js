ea2b7e536cdc6652991bbbae8f4d98cac0aed1c6
Konstantin <48102406+fomichovK@users.noreply.github.com>
2022-11-02 14:25:35 +0200

    finish 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
 
+const { readdir, copyFile } = require('node:fs/promises');
+const fs = require('fs');
+
+const newDir = fs.mkdir('./04-copy-directory/file-copy', (err) => {
+  if (err) console.log(err);
+});
+
+(async () => {
+  const files = await readdir('./04-copy-directory/files');
+  for (const file of files) {
+    try {
+      await copyFile('./04-copy-directory/files/' + file, './04-copy-directory/file-copy/' + file);
+      console.log(' was copied');
+    } catch {
+      console.log(' The file could not be copied');
+    }
+  }
+})();
